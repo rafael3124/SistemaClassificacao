d3dbd8ec7e4f1bdfc9cdc814939768b68787ac6d
Rafael Araujo <rafael3124@gmail.com>
2022-06-05 00:09:19 -0300

    Create contagem.js

---
+++ contagem.js
@@ -0,0 +1,11 @@
+let numeroAlunos = 10;
+
+for(let contador = 0; contador < numeroAlunos; contador++){
+    if(contador == 0){
+        console.log("O numero é ZERO")
+    }else if(contador % 2 == 0){
+        console.log(`O numero ${contador} é PAR`)
+    }else if(contador % 2 == 1){
+        console.log(`O numero ${contador} é IMPAR`)
+    }
+}
